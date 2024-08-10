49856d64d70dad7450b7fd375f59fe2ada5c5ae0
blazejzj <opexrrator@gmail.com>
2024-08-10 16:54:28 +0200

    FEAT: message + sound class, need to get some sounds

---
+++ src/modules/utils/message.js
@@ -0,0 +1,162 @@
+import Game from '../factories/battleManager';
+
+class MessageManager {
+  constructor() {
+    this.messages = {
+      welcome: [
+        'Ahoy, Captain!',
+        'Prepare our fleet by selecting the axis and placing the ships on the grid.',
+      ],
+      battleStartPlayer: [
+        "All systems are green. Let's give them a battle they'll never forget!",
+      ],
+      battleStartEnemy: [
+        "No mercy will be shown, just as your father showed none to mine.",
+      ],
+      enemyHit: [
+        "Direct hit, Captain! They're starting to take on water.",
+        'Another hit, sir! Their ship is taking serious damage.',
+        'Bullseye! We’re breaking their defenses.',
+        'Captain, that shot landed perfectly. The enemy is faltering.',
+        "We're hitting them hard, Captain. They won't last long.",
+        'Excellent shot! The enemy ship is heavily damaged.',
+        "That's a solid hit, Captain. They're on the brink!",
+        'Another successful hit, sir! The enemy ship is reeling.',
+        'The enemy ship took that hit hard! Well done, Captain.',
+        'Target struck! Their ship is barely holding together.',
+      ],
+      enemySunk: [
+        'Well done, Captain! That enemy vessel is sinking fast.',
+        'Enemy ship down! Another victory for us, Captain.',
+        "Captain, we've sunk their ship. One less threat to worry about.",
+        'Another one bites the dust, Captain. Enemy ship has been sunk.',
+        'Direct hit! The enemy ship is going under.',
+        'Victory, Captain! That enemy ship is no more.',
+        'The enemy ship has been sent to the bottom of the ocean. Well done.',
+        'That ship is toast, Captain. It’s going down!',
+        'The enemy ship has been obliterated. We’re unstoppable!',
+        'We’ve sent another ship to the depths. Victory is ours, Captain!',
+      ],
+      playerMiss: [
+        'So close, Captain! We’ll get them next time.',
+        'No luck with that shot, Captain.',
+        'Missed that one, but we’ll hit the next.',
+        'Our aim was off, but we’ll adjust.',
+        'No hit, Captain. The enemy evaded us.',
+        'That was a miss, but don’t lose hope, Captain.',
+        'The enemy slipped away this time, Captain.',
+        'We need to recalibrate our aim, Captain.',
+        'They dodged our shot, but we’ll strike again soon.',
+        'Not quite, Captain. We’ll nail them with the next one.',
+      ],
+      playerHit: [
+        'Your ship is under fire!',
+        'Brace yourself! We’ve just scored a hit.',
+        'Direct hit on your vessel! Damage sustained.',
+        "You're in our sights now!",
+        'Another hit! Your ship is taking on water.',
+        'Gotcha! Your defenses are failing.',
+        'Bullseye! Your ship won’t last much longer.',
+        "You're taking serious damage! Ready to surrender?",
+        'We’ve got you cornered. Prepare for the final blow!',
+        'Your ship is barely holding together. Time to give up?',
+      ],
+      playerSunk: [
+        "Your ship's going down. Better start swimming!",
+        'Another one of your ships bites the dust.',
+        'Your ship has been sunk! What’s next, Captain?',
+        'We’ve sent your ship to the bottom of the sea!',
+        'Another one of your ships meets a watery grave.',
+        "It's over for that ship. You’re running out of options!",
+        'Your fleet is dwindling, Captain. Another ship down.',
+        'The ocean claims another one of your ships. Too bad!',
+        'Your ship couldn’t withstand the assault. It’s done for.',
+        'Another ship sunk! How long can you last, Captain?',
+      ],
+      enemyMiss: [
+        'Missed me, missed me!',
+        'That shot went wide. Your luck is holding, Captain.',
+        "They can't hit us, Captain! We’re too good!",
+        'Another miss from the enemy! They’re losing their touch.',
+        'Their aim was off! We’re safe, Captain.',
+        "They didn't land that one. We’re still in the fight.",
+        "They missed again! Let’s show them how it’s done.",
+        'The enemy can’t seem to hit us. We’ve got the upper hand!',
+        'Another enemy shot, another miss. We’ve got this!',
+        'They missed! Now it’s our turn to strike.',
+      ],
+      noComment: ['...'],
+      playerWin: [
+        'Victory is ours, Captain! You truly are the ruler of the seas.',
+      ],
+      enemyWin: [
+        'Defeated! This is for my father. You were no match for me.',
+      ],
+    };
+  }
+
+  getWelcomeMessage() {
+    const playerName = Game.getState().getPlayer().getName();
+    this.messages.welcome[0] += ` ${playerName}!`;
+    return this.messages.welcome;
+  }
+
+  getBattleStartMessage() {
+    const playerName = Game.getState().getPlayer().getName();
+    return [`${playerName}, ${this.messages.battleStartPlayer[0]}`];
+  }
+
+  getNewEnemyBattleStartMessage() {
+    return this.messages.battleStartEnemy;
+  }
+
+  getNewEnemyHitMessage(previousMessage) {
+    return this.getRandomMessage(this.messages.enemyHit, previousMessage);
+  }
+
+  getNewEnemySunkMessage(previousMessage) {
+    return this.getRandomMessage(this.messages.enemySunk, previousMessage);
+  }
+
+  getNewPlayerMissMessage(previousMessage) {
+    return this.getRandomMessage(this.messages.playerMiss, previousMessage);
+  }
+
+  getNewPlayerHitMessage(previousMessage) {
+    return this.getRandomMessage(this.messages.playerHit, previousMessage);
+  }
+
+  getNewPlayerSunkMessage(previousMessage) {
+    return this.getRandomMessage(this.messages.playerSunk, previousMessage);
+  }
+
+  getNewEnemyMissMessage(previousMessage) {
+    return this.getRandomMessage(this.messages.enemyMiss, previousMessage);
+  }
+
+  getNoCommentMessage() {
+    return this.messages.noComment;
+  }
+
+  getPlayerWinMessage() {
+    return this.messages.playerWin;
+  }
+
+  getEnemyWinMessage() {
+    return this.messages.enemyWin;
+  }
+
+  getRandomMessage(messageArray, previousMessage) {
+    let newMessage = previousMessage;
+    while (newMessage === previousMessage) {
+      newMessage = messageArray[this.randomIndex(messageArray.length)];
+    }
+    return newMessage;
+  }
+
+  randomIndex(max) {
+    return Math.floor(Math.random() * max);
+  }
+}
+
+module.exports = new MessageManager();
---
+++ src/modules/utils/sound.js
@@ -0,0 +1,55 @@
+// ASSETS
+import shotSound from '../../assets/sounds/shot.mp3';
+import hitSound from '../../assets/sounds/hit.mp3';
+import missSound from '../../assets/sounds/miss.mp3';
+
+class SoundManager {
+  constructor() {
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  }
+
+  playSound(soundUrl) {
+    const request = new XMLHttpRequest();
+    request.open('GET', soundUrl, true);
+    request.responseType = 'arraybuffer';
+    request.onload = () => {
+      this.audioCtx.decodeAudioData(request.response, (buffer) => {
+        const source = this.audioCtx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(this.audioCtx.destination);
+        source.start(0);
+      });
+      this.audioCtx.resume();
+    };
+    request.send();
+  }
+
+  shot() {
+    this.playSound(shotSound);
+  }
+
+  hit() {
+    this.playSound(hitSound);
+  }
+
+  miss() {
+    this.playSound(missSound);
+  }
+
+  async background() {
+    const audioModule = await import('../../assets/sounds/backgroundOcean.mp3');
+    const audio = new Audio(audioModule.default);
+    audio.loop = true;
+    audio.play();
+  }
+
+  BackgroundOnFirstTouch() {
+    if (/Android|iPhone/i.test(navigator.userAgent)) {
+      document.addEventListener('touchstart', this.background.bind(this), { once: true });
+    } else {
+      document.addEventListener('click', this.background.bind(this), { once: true });
+    }
+  }
+}
+
+module.exports = new SoundManager();
