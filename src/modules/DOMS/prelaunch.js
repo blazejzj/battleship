b25277f9eb9e0996e5843bcfd817e0803da75a1b
blazejzj <opexrrator@gmail.com>
2024-11-19 21:38:30 +0100

    feat: added animated startup + background

--- src/modules/DOMS/prelaunch.js
+++ src/modules/DOMS/prelaunch.js
@@ -1,65 +1,64 @@
-import helper from './helper';
+import helper from "./helper";
 
 class Prelaunch {
+    loadCard() {
+        const app = document.getElementById("app");
+        app.classList.add("pregame");
 
-  loadCard() {
-    const app = document.getElementById('app');
-    app.classList.add('pregame');
+        const pregameCard = this.createPregameCard();
+        helper.appendAll(app, [pregameCard]);
+    }
 
-    const pregameCard = this.createPregameCard();
-    helper.appendAll(app, [pregameCard]);
-  }
+    createPregameCard() {
+        const section = helper.create("section", { className: "pregame-card" });
+        const title = this.createTitle();
+        const nameForm = this.createNameForm();
+        const playNowButton = this.createPlayNowButton();
 
-  createPregameCard() {
-    const section = helper.create('section', { className: 'pregame-card' });
-    const title = this.createTitle();
-    const nameForm = this.createNameForm();
-    const playNowButton = this.createPlayNowButton();
+        helper.appendAll(section, [title, nameForm, playNowButton]);
 
-    helper.appendAll(section, [title, nameForm, playNowButton]);
+        return section;
+    }
 
-    return section;
-  }
+    createTitle() {
+        return helper.create("h1", { textContent: "BATTLESHIP" });
+    }
 
-  createTitle() {
-    return helper.create('h1', { textContent: 'BATTLESHIP' });
-  }
+    createNameForm() {
+        const container = helper.create("div", { className: "name-form" });
 
-  createNameForm() {
-    const container = helper.create('div', { className: 'name-form' });
+        const input = helper.create("input", {
+            type: "text",
+            id: "name-input",
+            className: "name-input",
+            placeholder: "Enter your name ...",
+            minLength: 0,
+            maxLength: 15,
+        });
 
-    const input = helper.create('input', {
-      type: 'text',
-      id: 'name-input',
-      className: 'name-input',
-      placeholder: 'Captain name',
-      minLength: 0,
-      maxLength: 15,
-    });
+        const border = helper.create("span", { className: "input-border" });
 
-    const border = helper.create('span', { className: 'input-border' });
+        helper.appendAll(container, [input, border]);
 
-    helper.appendAll(container, [input, border]);
+        return container;
+    }
 
-    return container;
-  }
+    createPlayNowButton() {
+        const button = helper.create("button", {
+            type: "button",
+            id: "play-now-button",
+            className: "play-now-button",
+        });
 
-  createPlayNowButton() {
-    const button = helper.create('button', {
-      type: 'button',
-      id: 'play-now-button',
-      className: 'play-now-button',
-    });
+        const text = helper.create("span", {
+            className: "text-play-button",
+            textContent: "PREPARE FOR BATTLE!",
+        });
 
-    const text = helper.create('span', {
-      className: 'text-play-button',
-      textContent: 'ENTER COMBAT',
-    });
+        button.appendChild(text);
 
-    button.appendChild(text);
-
-    return button;
-  }
+        return button;
+    }
 }
 
 export default new Prelaunch();
