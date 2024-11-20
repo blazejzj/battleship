15a3dd02327cabff790c8641fa89dc10195c0570
blazejzj <opexrrator@gmail.com>
2024-11-20 18:37:25 +0100

    feat: added and fixed sounds

--- src/modules/utils/sound.js
+++ src/modules/utils/sound.js
@@ -1,89 +1,94 @@
-import shotSound from '../../assets/sounds/shot.mp3';
-import hitSound from '../../assets/sounds/hit.mp3';
-import missSound from '../../assets/sounds/miss.mp3';
-import backgroundOcean from '../../assets/sounds/backgroundOcean.mp3';
+import shotSound from "../../assets/sounds/shot.mp3";
+import hitSound from "../../assets/sounds/hit.flac";
+import missSound from "../../assets/sounds/miss.mp3";
+import backgroundOcean from "../../assets/sounds/backgroundOcean.wav";
 
 class SoundManager {
-  constructor() {
-    this.audioCtx = null;
-    this.backgroundSource = null; 
-  }
-
-  initAudioContext() {
-    if (!this.audioCtx) {
-      this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    } else if (this.audioCtx.state === 'suspended') {
-      this.audioCtx.resume();
+    constructor() {
+        this.audioCtx = null;
+        this.backgroundSource = null;
     }
-  }
 
-  async playSound(soundUrl) {
-    this.initAudioContext();
-  
-    try {
-      const response = await fetch(soundUrl);
-      const arrayBuffer = await response.arrayBuffer();
-      const audioBuffer = await this.audioCtx.decodeAudioData(arrayBuffer);
-  
-      const source = this.audioCtx.createBufferSource();
-      source.buffer = audioBuffer;
-      source.connect(this.audioCtx.destination);
-      source.start(0);
-    } catch (error) {
-      console.error('Error playing sound:', error);
+    initAudioContext() {
+        if (!this.audioCtx) {
+            this.audioCtx = new (window.AudioContext ||
+                window.webkitAudioContext)();
+        } else if (this.audioCtx.state === "suspended") {
+            this.audioCtx.resume();
+        }
     }
-  }
 
-  shot() {
-    this.playSound(shotSound);
-  }
+    async playSound(soundUrl) {
+        this.initAudioContext();
 
-  hit() {
-    this.playSound(hitSound);
-  }
+        try {
+            const response = await fetch(soundUrl);
+            const arrayBuffer = await response.arrayBuffer();
+            const audioBuffer = await this.audioCtx.decodeAudioData(
+                arrayBuffer
+            );
 
-  miss() {
-    this.playSound(missSound);
-  }
-
-  async playBackground() {
-    this.initAudioContext(); // Ensure AudioContext is initialized
-    const response = await fetch(backgroundOcean);
-    const arrayBuffer = await response.arrayBuffer();
-    this.audioCtx.decodeAudioData(arrayBuffer, (buffer) => {
-      this.backgroundSource = this.audioCtx.createBufferSource();
-      this.backgroundSource.buffer = buffer;
-      this.backgroundSource.loop = true;
-      this.backgroundSource.connect(this.audioCtx.destination);
-      this.backgroundSource.start(0);
-    });
-  }
-
-  BackgroundOnFirstTouch() {
-    const unlockAudio = async () => {
-      try {
-        await this.playBackground(); // Play background audio
-      } catch (error) {
-        console.error('Error playing background audio:', error);
-      } finally {
-        document.removeEventListener('click', unlockAudio);
-        document.removeEventListener('touchstart', unlockAudio);
-      }
-    };
-
-    if (/Android|iPhone/i.test(navigator.userAgent)) {
-      document.addEventListener('touchstart', unlockAudio, { once: true });
-    } else {
-      document.addEventListener('click', unlockAudio, { once: true });
+            const source = this.audioCtx.createBufferSource();
+            source.buffer = audioBuffer;
+            source.connect(this.audioCtx.destination);
+            source.start(0);
+        } catch (error) {
+            console.error("Error playing sound:", error);
+        }
     }
-  }
 
-  stopBackground() {
-    if (this.backgroundSource) {
-      this.backgroundSource.stop();
-      this.backgroundSource = null;
+    shot() {
+        this.playSound(shotSound);
     }
-  }
+
+    hit() {
+        this.playSound(hitSound);
+    }
+
+    miss() {
+        this.playSound(missSound);
+    }
+
+    async playBackground() {
+        this.initAudioContext();
+        const response = await fetch(backgroundOcean);
+        const arrayBuffer = await response.arrayBuffer();
+        this.audioCtx.decodeAudioData(arrayBuffer, (buffer) => {
+            this.backgroundSource = this.audioCtx.createBufferSource();
+            this.backgroundSource.buffer = buffer;
+            this.backgroundSource.loop = true;
+            this.backgroundSource.connect(this.audioCtx.destination);
+            this.backgroundSource.start(0);
+        });
+    }
+
+    BackgroundOnFirstTouch() {
+        const unlockAudio = async () => {
+            try {
+                await this.playBackground();
+            } catch (error) {
+                console.error("Error playing background audio:", error);
+            } finally {
+                document.removeEventListener("click", unlockAudio);
+                document.removeEventListener("touchstart", unlockAudio);
+            }
+        };
+
+        if (/Android|iPhone/i.test(navigator.userAgent)) {
+            document.addEventListener("touchstart", unlockAudio, {
+                once: true,
+            });
+        } else {
+            document.addEventListener("click", unlockAudio, { once: true });
+        }
+    }
+
+    stopBackground() {
+        if (this.backgroundSource) {
+            this.backgroundSource.stop();
+            this.backgroundSource = null;
+        }
+    }
 }
 
 export default new SoundManager();
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,40 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/modules/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, './dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']  
-      },
-      {
-        test: /\.scss$/, 
-        use: [
-          'style-loader',
-          'css-loader',   
-          'sass-loader' 
+    entry: "./src/modules/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "./dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(mp3|wav|ogg|flac)$/i,
+                type: "asset/resource",
+            },
         ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(mp3|wav|ogg)$/i, 
-        type: 'asset/resource',
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html'
-    }),
-  ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html",
+        }),
+    ],
 };
