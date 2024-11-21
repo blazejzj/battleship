da3dc90be29576b37e68797db9a1dca8318e052b
blazejzj <opexrrator@gmail.com>
2024-11-21 11:11:10 +0100

    feat: updated eslint rules to exclude 'this' in classes

--- src/modules/factories/ship.js
+++ src/modules/factories/ship.js
@@ -1,77 +1,77 @@
 class Ship {
-  constructor(shipName, shipLength) {
-    this.name = shipName;
-    this.length = shipLength;
-    this.hits = 0;
-    this.isSunk = false;
-    this.isFound = false;
-  }
-
-  // Getters
-  getName() {
-    return this.name;
-  }
-
-  getFound() {
-    return this.isFound;
-  }
-
-  getSunk() {
-    return this.isSunk;
-  }
-
-  getLength() {
-    return this.length;
-  }
-  
-  getHits() {
-    return this.hits;
-  }
-
-  // Setters
-  setName(newName) {
-    this.name = newName;
-  }
-
-  setFound(newFound) {
-    this.isFound = newFound;
-  }
-
-  setSunk(newSunk) {
-    this.isSunk = newSunk;
-  }
-
-  setLength(newLength) {
-    this.length = newLength;
-  }
-
-  setHits(newHits) {
-    this.hits = newHits;
-  }
-
-  // Methods
-  hit() {
-    this.hits += 1;
-    if (this.hits === this.length) {
-      this.sink(); 
+    constructor(shipName, shipLength) {
+        this.name = shipName;
+        this.length = shipLength;
+        this.hits = 0;
+        this.isSunk = false;
+        this.isFound = false;
     }
-  }
 
-  sink() {
-    this.isSunk = true;
-  }
+    // Getters
+    getName() {
+        return this.name;
+    }
 
-  toggleFound() {
-    this.isFound = !this.isFound; 
-  }
+    getFound() {
+        return this.isFound;
+    }
 
-  resetFound() {
-    this.isFound = false;
-  }
+    getSunk() {
+        return this.isSunk;
+    }
 
-  found() {
-    this.isFound = true;
-  }
+    getLength() {
+        return this.length;
+    }
+
+    getHits() {
+        return this.hits;
+    }
+
+    // Setters
+    setName(newName) {
+        this.name = newName;
+    }
+
+    setFound(newFound) {
+        this.isFound = newFound;
+    }
+
+    setSunk(newSunk) {
+        this.isSunk = newSunk;
+    }
+
+    setLength(newLength) {
+        this.length = newLength;
+    }
+
+    setHits(newHits) {
+        this.hits = newHits;
+    }
+
+    // Methods
+    hit() {
+        this.hits += 1;
+        if (this.hits === this.length) {
+            this.sink();
+        }
+    }
+
+    sink() {
+        this.isSunk = true;
+    }
+
+    toggleFound() {
+        this.isFound = !this.isFound;
+    }
+
+    resetFound() {
+        this.isFound = false;
+    }
+
+    found() {
+        this.isFound = true;
+    }
 }
 
 export default Ship;
