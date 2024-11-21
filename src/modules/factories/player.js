db04dd0a9e605b77d76b8579e46380eefb60d3dc
blazejzj <opexrrator@gmail.com>
2024-11-21 11:09:34 +0100

    refactor: remove redundant comments

--- src/modules/factories/player.js
+++ src/modules/factories/player.js
@@ -1,175 +1,170 @@
-import Gameboard from './gameBoard'
-import Ship from './ship'
+import Gameboard from "./gameBoard";
+import Ship from "./ship";
 
 class Player {
-  constructor(playerAlias, playerType) {
-    this.alias = playerAlias;
-    this.type = playerType; // Playertype -> human or computer
-    this.grid = new Gameboard(); // initialize a new gameboard
-    this.attackQueue = [];
-    this.moves = 0;
-  }
+    constructor(playerAlias, playerType) {
+        this.alias = playerAlias;
+        this.type = playerType; // Playertype -> human or computer
+        this.grid = new Gameboard(); // initialize a new gameboard
+        this.attackQueue = [];
+        this.moves = 0;
+    }
 
-  // Getters
+    // Getters
 
-  getAlias() {
-    return this.alias;
-  }
+    getAlias() {
+        return this.alias;
+    }
 
-  getGrid() {
-    return this.grid;
-  }
+    getGrid() {
+        return this.grid;
+    }
 
-  getType() {
-    return this.type;
-  }
+    getType() {
+        return this.type;
+    }
 
-  // Setters
+    // Setters
 
-  setAlias(newAlias) {
-    this.alias = newAlias;
-  }
+    setAlias(newAlias) {
+        this.alias = newAlias;
+    }
 
-  // Methods
-  isFieldEmpty(coordinate) {
-    const [x, y] = coordinate;
-    // checks if a specific field on the board has already been hit or missed
-    return (
-      this.getGrid().getBoard()[x][y] !== 'miss' &&
-      this.getGrid().getBoard()[x][y] !== 'hit'
-    );
-  }
+    // Methods
+    isFieldEmpty(coordinate) {
+        const [x, y] = coordinate;
+        return (
+            this.getGrid().getBoard()[x][y] !== "miss" &&
+            this.getGrid().getBoard()[x][y] !== "hit"
+        );
+    }
 
-  hasLost() {
-    return this.getGrid().getFleet().every((ship) => ship.getSunk() === true);
-  }
+    hasLost() {
+        return this.getGrid()
+            .getFleet()
+            .every((ship) => ship.getSunk() === true);
+    }
 
-  autoPosition() {
-    const ships = [
-      { name: 'carrier', length: 5 },
-      { name: 'battleship', length: 4 },
-      { name: 'cruiser', length: 3 },
-      { name: 'submarine', length: 3 },
-      { name: 'destroyer', length: 2 }
-    ];
-  
-    ships.forEach(ship => {
-      let placed = false;
-  
-      while (!placed) {
-        const direction = this.randomDirection();
-        const row = this.randomCoordinate();
-        const col = this.randomCoordinate();
-        const currentShip = new Ship(ship.name, ship.length);
-  
-        if (direction === 'x') {
-          placed = this.getGrid().placeX(currentShip, [row, col]);
-        } else {
-          placed = this.getGrid().placeY(currentShip, [row, col]);
+    autoPosition() {
+        const ships = [
+            { name: "carrier", length: 5 },
+            { name: "battleship", length: 4 },
+            { name: "cruiser", length: 3 },
+            { name: "submarine", length: 3 },
+            { name: "destroyer", length: 2 },
+        ];
+
+        ships.forEach((ship) => {
+            let placed = false;
+
+            while (!placed) {
+                const direction = this.randomDirection();
+                const row = this.randomCoordinate();
+                const col = this.randomCoordinate();
+                const currentShip = new Ship(ship.name, ship.length);
+
+                if (direction === "x") {
+                    placed = this.getGrid().placeX(currentShip, [row, col]);
+                } else {
+                    placed = this.getGrid().placeY(currentShip, [row, col]);
+                }
+            }
+        });
+    }
+
+    computerTurn() {
+        let invalidMove = true;
+        let x;
+        let y;
+
+        while (invalidMove) {
+            if (this.attackQueue.length > 1)
+                [x, y] = this.getRandomAndRemove(this.attackQueue);
+            else {
+                x = this.randomCoordinate();
+                y = this.randomCoordinate();
+            }
+            if (this.isFieldEmpty([x, y])) {
+                invalidMove = false;
+                this.getGrid().receiveAttack([x, y]);
+            }
         }
-      }
-    });
-  };
 
-  computerTurn() {
-    let invalidMove = true;
-    let x;
-    let y;
-
-    // loop until a valid move (not already hit or missed) is found
-    while (invalidMove) {
-      if (this.attackQueue.length > 1)
-        // if there are potential targets in the queue, pick one
-        [x, y] = this.getRandomAndRemove(this.attackQueue);
-      else {
-        // otherwise, choose random coordinates
-        x = this.randomCoordinate();
-        y = this.randomCoordinate();
-      }
-
-      // check if the chosen field hasn't been attacked yet
-      if (this.isFieldEmpty([x, y])) {
-        invalidMove = false;
-        // attack the field on the board
-        this.getGrid().receiveAttack([x, y]);
-      }
+        this.populateQueue(x, y);
+        this.moves++;
+        return [x, y];
     }
 
-    this.populateQueue(x, y)
-    this.moves++;
-    return [x, y];
-  }
+    populateQueue(row, col) {
+        if (this.attackQueue.length === 1) {
+            this.attackQueue = [];
+        }
 
-  populateQueue(row, col) {
-    if (this.attackQueue.length === 1) {
-      this.attackQueue = [];
-    }
+        if (this.getGrid().getBoard()[row][col] === "miss") {
+            return;
+        }
 
-    if (this.getGrid().getBoard()[row][col] === 'miss') {
-      return;
-    }
+        let initial = false;
 
-    let initial = false;
+        if (this.attackQueue.length === 0) {
+            this.attackQueue.push([row, col]);
+            initial = true;
+        }
 
-    if (this.attackQueue.length === 0) {
-      this.attackQueue.push([row, col]);
-      initial = true;
-    }
-
-    // Add adjacent fields to the queue
-    const adjacentFields = [
-      [row - 1, col], // above
-      [row + 1, col], // below
-      [row, col - 1], // left
-      [row, col + 1]  // right
-    ];
-
-    adjacentFields.forEach(([r, c]) => {
-      if (r >= 0 && r <= 9 && c >= 0 && c <= 9) {
-        this.attackQueue.push([r, c]);
-      }
-    });
-
-    // Filter the queue to keep only the fields that are in the same row or column as the first hit
-    if (this.attackQueue.length > 1 && !initial) {
-      const [firstRow, firstCol] = this.attackQueue[0];
-      if (row === firstRow) {
-        this.attackQueue = [
-          this.attackQueue[0],
-          ...this.attackQueue.slice(1).filter(([r]) => r === row)
+        // Add adjacent fields to the queue
+        const adjacentFields = [
+            [row - 1, col], // above
+            [row + 1, col], // below
+            [row, col - 1], // left
+            [row, col + 1], // right
         ];
-      } else if (col === firstCol) {
-        this.attackQueue = [
-          this.attackQueue[0],
-          ...this.attackQueue.slice(1).filter(([, c]) => c === col)
-        ];
-      }
+
+        adjacentFields.forEach(([r, c]) => {
+            if (r >= 0 && r <= 9 && c >= 0 && c <= 9) {
+                this.attackQueue.push([r, c]);
+            }
+        });
+
+        // Filter the queue to keep only the fields that are in the same row or column as the first hit
+        if (this.attackQueue.length > 1 && !initial) {
+            const [firstRow, firstCol] = this.attackQueue[0];
+            if (row === firstRow) {
+                this.attackQueue = [
+                    this.attackQueue[0],
+                    ...this.attackQueue.slice(1).filter(([r]) => r === row),
+                ];
+            } else if (col === firstCol) {
+                this.attackQueue = [
+                    this.attackQueue[0],
+                    ...this.attackQueue.slice(1).filter(([, c]) => c === col),
+                ];
+            }
+        }
     }
-  }
 
-  playTurn(coordinate) {
-    const [x, y] = coordinate;
-    if (this.isFieldEmpty([x, y])) {
-      this.getGrid().receiveAttack([x, y]);
-      this.moves++;
-    };
-  }
+    playTurn(coordinate) {
+        const [x, y] = coordinate;
+        if (this.isFieldEmpty([x, y])) {
+            this.getGrid().receiveAttack([x, y]);
+            this.moves++;
+        }
+    }
 
-  randomCoordinate() {
-    return Math.floor(Math.random() * (9 + 1));
-  }
+    randomCoordinate() {
+        return Math.floor(Math.random() * (9 + 1));
+    }
 
-  randomDirection() {
-    const directions = ['x', 'y'];
-    return directions[Math.round(Math.random())];
-  }
+    randomDirection() {
+        const directions = ["x", "y"];
+        return directions[Math.round(Math.random())];
+    }
 
-  getRandomAndRemove(array) {
-    const randomIndex = Math.floor(Math.random() * (array.length - 1)) + 1;
-    const randomElement = array[randomIndex];
-    array.splice(randomIndex, 1);
-    return randomElement;
-  }
+    getRandomAndRemove(array) {
+        const randomIndex = Math.floor(Math.random() * (array.length - 1)) + 1;
+        const randomElement = array[randomIndex];
+        array.splice(randomIndex, 1);
+        return randomElement;
+    }
 }
 
 export default Player;
