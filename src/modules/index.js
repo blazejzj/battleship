63a38a0b96926c48fe32a9426931bc5c95ff637c
blazejzj <opexrrator@gmail.com>
2024-11-21 11:12:36 +0100

    fix: fixed import typos

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,6 @@
-import view from './DOMS/view'
-import Sound from '../modules/utils/sound'
-import '../styles/main.scss'
+import view from "./DOMS/view";
+import Sound from "./utils/sound";
+import "../styles/main.scss";
 
 view.loadContent();
-Sound.BackgroundOnFirstTouch()
+Sound.BackgroundOnFirstTouch();
